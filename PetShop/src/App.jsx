005c760bcca41dfcfa4c09921c4ec4597850b3d4
SHANMUKH SAI <saishanmuk28@gmail.com>
2025-03-20 21:23:34 +0530

    files add commit

--- PetShop/src/App.jsx
+++ PetShop/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>Change this file to run PetShop\src\app\layout.js As main file </code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
